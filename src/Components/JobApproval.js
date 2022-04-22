c11f79c5cafb0bacb5da0d59165f6ba844447b34
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-04-22 22:17:01 +0530

    Added a component

---
+++ src/Components/JobApproval.js
@@ -0,0 +1,66 @@
+function JobApproval() {
+  return (
+    <>
+      <div
+        style="min-height:100vh;background-color : #E8F3FE"
+        class="items-center justify-center pt-11 lg:flex"
+      >
+        <div class="flex flex-col items-center rounded-md bg-white shadow-2xl lg:w-2/3">
+          <div
+            class="w-full rounded-md text-center font-sans text-lg font-semibold text-white"
+            style="background-color:#003979"
+          >
+            <p>Job Approval</p>
+          </div>
+          <div class="m-3 flex w-auto flex-grow rounded-lg shadow-lg duration-300 hover:-translate-y-1 hover:scale-110 sm:h-20 sm:w-4/5">
+            <p class="w-auto pt-5 text-center">1.</p>
+            <span class="w-auto p-0 sm:p-3">
+              <p class="font-bold">Job Nameeeeeeeeeeeeeeeeee</p>
+              <p class="font-semibold text-gray-600">Posted By</p>
+            </span>
+            <div class="flex w-auto flex-grow items-center justify-end text-xs">
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-green-300 sm:h-10 sm:w-20">
+                Approve
+              </button>
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-red-300 sm:h-10 sm:w-20">
+                Reject
+              </button>
+            </div>
+          </div>
+          <div class="m-3 flex w-auto flex-grow rounded-lg shadow-lg duration-300 hover:-translate-y-1 hover:scale-110 sm:h-20 sm:w-4/5">
+            <p class="w-auto pt-5 text-center">1.</p>
+            <span class="w-auto p-0 sm:p-3">
+              <p class="font-bold">Job Nameeeeeeeeeeeeeeeeee</p>
+              <p class="font-semibold text-gray-600">Posted By</p>
+            </span>
+            <div class="flex w-auto flex-grow items-center justify-end text-xs">
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-green-300 sm:h-10 sm:w-20">
+                Approve
+              </button>
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-red-300 sm:h-10 sm:w-20">
+                Reject
+              </button>
+            </div>
+          </div>
+          <div class="m-3 flex w-auto flex-grow rounded-lg shadow-lg duration-300 hover:-translate-y-1 hover:scale-110 sm:h-20 sm:w-4/5">
+            <p class="w-auto pt-5 text-center">1.</p>
+            <span class="w-auto p-0 sm:p-3">
+              <p class="font-bold">Job Nameeeeeeeeeeeeeeeeee</p>
+              <p class="font-semibold text-gray-600">Posted By</p>
+            </span>
+            <div class="flex w-auto flex-grow items-center justify-end text-xs">
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-green-300 sm:h-10 sm:w-20">
+                Approve
+              </button>
+              <button class="m-2 h-auto w-16 rounded-full border-2 p-2 hover:border-gray-400 hover:bg-red-300 sm:h-10 sm:w-20">
+                Reject
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default JobApproval;
