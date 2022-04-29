bcb564785d610cdecd3b89eb61191443b63c359a
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-04-30 02:02:37 +0530

    Added newsLetterPopup component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
+import NewsLetterPopup from "./Components/NewsLetterPopup";
+
 export default function App() {
   const arr = [1, 2, 3, 4];
   return (
-    <div className="grid lg:grid-cols-2 gap-3">
-      {arr.map((val) => {
-        return (
-          <>
-            <div className="flex justify-center">
-              <div
-                className="bg-gray-300 rounded-md overflow-hidden shadow-sm "
-                style={{
-                  border: "1px solid black",
-                }}
-              >
-                <img
-                  src="https://wallpapercave.com/wp/wp5348399.jpg"
-                  className="w-full h-30 sm:h-50 object-cover"
-                />
-                <h2 className="m-4">Hello</h2>
-              </div>
-            </div>
-          </>
-        );
-      })}
-    </div>
+    <>
+      <NewsLetterPopup />
+    </>
   );
 }
 
@@ -52,5 +35,28 @@ Grid
         <div style={{ border: "2px solid blue" }}>Hello</div>
       </div>
       <div style={{ border: "2px solid black" }}>Hello</div>
+    </div>
+
+        <div className="grid lg:grid-cols-2 gap-3">
+      {arr.map((val) => {
+        return (
+          <>
+            <div className="flex justify-center">
+              <div
+                className="bg-gray-300 rounded-md overflow-hidden shadow-sm "
+                style={{
+                  border: "1px solid black",
+                }}
+              >
+                <img
+                  src="https://wallpapercave.com/wp/wp5348399.jpg"
+                  className="w-full h-30 sm:h-50 object-cover"
+                />
+                <h2 className="m-4">Hello</h2>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
 */
